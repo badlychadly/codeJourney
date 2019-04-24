3c02c35755756e2a36eecffe2223c37e3561474f
badlychadly <chadmontoya21@yahoo.com>
2019-04-24 12:39:36 -0700

    create action working in posts_controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,17 +1,19 @@
 class Api::PostsController < ApplicationController
+    before_action :find_author
 
     def index
         render json: Post.all
     end
 
     def create
-        binding.pry
+        post = @author.posts.create(post_params)
+        render json: post
     end
 
     private 
 
     def post_params
-        params.require(:post).permit(:tite, :sub_title, :body)
+        params.require(:post).permit(:title, :sub_title, :body)
     end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+    private
+    def find_author
+        @author ||= Author.first
+    end
 end
