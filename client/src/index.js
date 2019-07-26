f06924e0c45e39c50298c8a27228e71a093c5a68
badlychadly <chadmontoya21@yahoo.com>
2019-07-26 12:53:08 -0700

    add dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
+require('dotenv').config()
 
 ReactDOM.render(
     <Provider store={store}>
